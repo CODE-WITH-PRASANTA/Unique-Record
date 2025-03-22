c85dfd85c1cdcb4cbb6428478332f95b818c22d8
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2025-03-22 22:09:18 +0530

    donw

--- backend/Server.js
+++ backend/Server.js
@@ -34,7 +34,7 @@ app.use("/api/team", teamMemberRoutes);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} 🚀`);
 });
